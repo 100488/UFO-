a5ead004a5b30148a07e03e85ea41ab390b18271
100488 <hollowme@live.jp>
2017-11-27 04:50:51 +0900

    UFOc 初回

---
+++ Assets/UFOcontroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOcontroller : MonoBehaviour {
+
+    public float speed = 1000.0f;
+    public Rigidbody rb;
+    public float dy;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") * speed;
+        float z = Input.GetAxis("Vertical") * speed;
+        rb.AddForce(x, 0, z);
+        if (Input.GetKeyDown("z"))
+        {
+            Debug.Log("z");
+            this.transform.position += new Vector3(0, dy, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("s");
+            GetComponent<Animator>().SetTrigger("catch");
+        }
+    }
+}
